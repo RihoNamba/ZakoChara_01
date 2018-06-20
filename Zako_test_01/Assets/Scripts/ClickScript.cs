1f77347b580e3495d26226ee6efc0ffcf42f195f
RihoNamba <dianthus1316@gmail.com>
2018-06-20 10:48:51 +0900

    ZakoChara_Test
    test

---
+++ Zako_test_01/Assets/Scripts/ClickScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickScript : MonoBehaviour {
+
+
+
+ 
+	   public void PopUp(){
+		 
+	
+        gameObject.transform.Translate(-1.5f,0.0f,0.0f);
+    }
+	   
+
+
+}
---
+++ Zako_test_01/Assets/Scripts/ObjectScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScript : MonoBehaviour {
+ 
+  
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	
+    // Update is called once per frame
+    void Update () {
+        if(Input.GetMouseButtonDown(0)){
+		
+            // マウスがクリックされたら以下を実行
+            ClickScript clickScript = GetComponent<ClickScript >();
+            clickScript .PopUp();
+        }
+    
+	}
+}
