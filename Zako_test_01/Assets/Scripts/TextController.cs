cdbeca25d8291f8d10b0ec86d805193fa3bbd607
RihoNamba <dianthus1316@gmail.com>
2018-06-25 12:00:55 +0900

    ZakoChara_試し中

--- Zako_test_01/Assets/Scripts/TextController.cs
+++ Zako_test_01/Assets/Scripts/TextController.cs
@@ -2,60 +2,51 @@
 using System.Collections;
 using UnityEngine.UI;
 
+// Messageboxのテキストを制御する
 public class TextController : MonoBehaviour 
 {
-	public string[] scenarios;
-	[SerializeField] Text uiText;
-	
 
 	[SerializeField][Range(0.001f, 0.3f)]
-	float intervalForCharacterDisplay = 0.05f;
+	public float intervalForCharacterDisplay = 0.05f;
 
 	private string currentText = string.Empty;
 	private float timeUntilDisplay = 0;
 	private float timeElapsed = 1;
-	private int currentLine = 0;
 	private int lastUpdateCharacter = -1;
 
-	// 文字の表示が完了しているかどうか
+	[SerializeField]
+	private Text _uiText;
+
 	public bool IsCompleteDisplayText 
 	{
 		get { return  Time.time > timeElapsed + timeUntilDisplay; }
 	}
 
-	void Start()
+	// 強制的に全文表示する
+	public void ForceCompleteDisplayText ()
 	{
-		SetNextLine();
+		timeUntilDisplay = 0;
 	}
+
+	// 次に表示する文字列をセットする
+	public void SetNextLine(string text)
+	{
+		currentText = text;
+		timeUntilDisplay = currentText.Length * intervalForCharacterDisplay;
+		timeElapsed = Time.time;
+		lastUpdateCharacter = -1;
+	}
+
+#region UNITY_CALLBACK	
 
 	void Update () 
 	{
-		// 文字の表示が完了してるならクリック時に次の行を表示する
-		if( IsCompleteDisplayText ){
-			if(currentLine < scenarios.Length && Input.GetMouseButtonDown(0)){
-				SetNextLine();
-			}
-		}else{
-		// 完了してないなら文字をすべて表示する
-			if(Input.GetMouseButtonDown(0)){
-				timeUntilDisplay = 0;
-			}
-		}
-
 		int displayCharacterCount = (int)(Mathf.Clamp01((Time.time - timeElapsed) / timeUntilDisplay) * currentText.Length);
 		if( displayCharacterCount != lastUpdateCharacter ){
-			uiText.text = currentText.Substring(0, displayCharacterCount);
+			_uiText.text = currentText.Substring(0, displayCharacterCount);
 			lastUpdateCharacter = displayCharacterCount;
 		}
 	}
 
-
-	void SetNextLine()
-	{
-		currentText = scenarios[currentLine];
-		timeUntilDisplay = currentText.Length * intervalForCharacterDisplay;
-		timeElapsed = Time.time;
-		currentLine ++;
-		lastUpdateCharacter = -1;
-	}
+#endregion
 }
